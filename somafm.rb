3bcc3ed471415e385a617e98c3a882309d8351e7
Tom Billingsley <tom.billingsley@gmail.com>
2016-01-23 15:00:04 -0500

    initial access control for Somafm class

--- somafm.rb
+++ somafm.rb
@@ -6,8 +6,19 @@ require 'nokogiri'
 # The SomafmPlaylist class is used to extract Soma FM streaming links
 # to a local M3u file for the conveninent playing with a standalone player
 class SomafmPlaylist
+  def channels
+    ndoc = Nokogiri::HTML(remote_html)
+    ndoc.xpath('//ul/li/a/@href').to_s.split('//')
+  end
+
   def groove_salad_uri
     'http://uwstream1.somafm.com:80'
+  end
+
+  private
+
+  def local_html
+    File.open('somafm.html', 'rb').read
   end
 
   def remote_html
@@ -16,14 +27,5 @@ class SomafmPlaylist
     request = Net::HTTP::Get.new(uri.request_uri)
     response = http.request(request)
     response.body
-  end
-
-  def local_html
-    File.open('somafm.html', 'rb').read
-  end
-
-  def channels
-    ndoc = Nokogiri::HTML(remote_html)
-    ndoc.xpath('//ul/li/a/@href').to_s.split('//')
   end
 end
