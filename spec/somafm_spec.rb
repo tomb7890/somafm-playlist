bbda9b023890c9af42a1460298d31296093ab4f5
Tom Billingsley <tom.billingsley@gmail.com>
2016-01-27 07:05:21 -0500

    create a seperate network group with describe

--- spec/somafm_spec.rb
+++ spec/somafm_spec.rb
@@ -1,7 +1,11 @@
 require_relative '../somafm'
-require_relative '../parser'
 
-describe 'Groove Salad Link' do
+def parsefile(p, filename)
+  page = File.open(filename, 'rb').read
+  p.parse(page)
+end
+
+describe 'local tests' do
   it 'detects a bad URI' do
     baduri = '##$$%%'
     expect { URI.parse(baduri) }.to raise_error(URI::InvalidURIError)
@@ -19,7 +23,30 @@ describe 'Groove Salad Link' do
 
   it 'finds seven h2 headings' do
     p = Parser.new
-    p.parse
+    parsefile(p, 'dsl.html')
     expect(p.formats.size).to eq 7
+  end
+
+  it 'makes the m3u file' do
+    s = SomafmPlaylist.new
+    s.make_m3u_file
+  end
+end
+
+describe 'network tests' do
+  it 'finds seven suburbsofgoa h2 headings' do
+    s = SomafmPlaylist.new
+    channels = s.channels
+    # expect(ch).to include('space')
+    channels = channels.sort_by { rand }
+    channels.each do |ch|
+      begin
+        formats = s.format_list_from_channel_name(ch)
+        expect(formats.size).to be >= 3
+      rescue StandardError, e
+        puts "The problematic thingy was #{ch}"
+        raise
+      end
+    end
   end
 end
