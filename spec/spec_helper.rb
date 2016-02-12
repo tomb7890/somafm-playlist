d3b283371d6d06f58596e3e28626cfd2ad2b4ddf
Tom Billingsley <tom.billingsley@gmail.com>
2016-02-12 06:53:31 -0500

    divert uri requests to local resources when test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,15 +12,16 @@ def local_html(f)
               else
                 "spec/data/somafm.com#{f.path}"
               end
-
   File.open(localfile, 'r')
 end
 
 RSpec.configure do |config|
   config.before(:each) do
-    stub_request(:any, %r{http://somafm.com}).to_return do |request|
-      response = local_html(request.uri)
-      { body: response, status: 200 }
+    if 'test' == ENV['ENV']
+      stub_request(:any, %r{http://somafm.com}).to_return do |request|
+        response = local_html(request.uri)
+        { body: response, status: 200 }
+      end
     end
   end
 end
