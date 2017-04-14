e51495892eabce3c3f9fd3e23ab7a8e3f146f67b
Tom Billingsley <tom.billingsley@gmail.com>
2017-04-14 10:57:59 -0400

    Delete unnecessary code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,4 @@
-# spec/spec_helper.rb
-require 'Rake'
 require 'webmock/rspec'
-
-def this_is_a_spec_task
-  # if Rake::Task.tasks.include?('spec')
-  true
-end
-
-if this_is_a_spec_task
-  WebMock.disable_net_connect!(allow_localhost: true)
-else
-  WebMock.allow_net_connect!
-end
 
 def local_html(f)
   localfile = if f.to_s == 'http://somafm.com:80/'
@@ -24,11 +11,9 @@ end
 
 RSpec.configure do |config|
   config.before(:each) do
-    if this_is_a_spec_task
-      stub_request(:any, %r{http://somafm.com}).to_return do |request|
-        response = local_html(request.uri)
-        { body: response, status: 200 }
-      end
+    stub_request(:any, %r{http://somafm.com}).to_return do |request|
+      response = local_html(request.uri)
+      { body: response, status: 200 }
     end
   end
 end
